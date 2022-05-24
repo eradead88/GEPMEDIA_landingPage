48adcdde9f34e393c976bd0231e336ed5f197f92
muflih23 <mfadli.muflih23@gmail.com>
2022-05-24 09:14:31 +0700

    commit build version

--- components/homepage_sidenav/sidenav.tsx
+++ components/homepage_sidenav/sidenav.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './sidenav.module.css'
 import openNav from './open.js'
 import closeNav from './close.js'
+import Link from 'next/link'
 
 function Sidenav() {
   return (
@@ -12,7 +13,7 @@ function Sidenav() {
             <a href='#keunggulan'>Keunggulan</a>
             <a href='#testimoni'>Testimoni</a>
             <a href='http://simakui.gepmedia.id'>Simak UI</a>
-            <a href='/utmbkipb'>UTMBK IPB</a>
+            <Link href='/utmbkipb'><a>UTMBK IPB</a></Link>
             <a href='https://tryout.gepmedia.id/#!/daftar'>Daftar</a>
             <a href='https://tryout.gepmedia.id/#!/login'>Login</a>
         </div>
